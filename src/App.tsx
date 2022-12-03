9fb97e4b1eeeb6d2dd66e8d47c8a882dabf39370
JuctTr <1301842163@qq.com>
2022-12-03 15:12:16 +0800

    feat(👻): 调整样式布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <></>;
+  const onAddNote = () => {};
+
+  const onDeleteNote = () => {};
+
+  const onUpdateNote = () => {};
+
+  const onSaveNote = () => {};
+
+  return (
+    <div className="App">
+      <div className="notes__sidebar">
+        <button onClick={onAddNote} className="notes__add" type="button">
+          添加新的笔记 📒
+        </button>
+        <button onClick={onDeleteNote} className="notes__add">
+          删除选中的笔记
+        </button>
+        <button onClick={onSaveNote} className="notes__add">
+          保存
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
